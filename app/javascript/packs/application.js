62469445c6e174ad8d7e01f54206367e2c0a3875
bodhisha <bodhisha@gmail.com>
2020-09-15 23:33:06 +0530

    add font awsome ,style sign in with google,navbar, flashes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import "../layouts/tailwind.css";
-
+import "@fortawesome/fontawesome-free/js/all.js";
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
