bec77c507b1041b058516b58632349eafe575dc0
bodhisha <bodhisha@gmail.com>
2020-09-21 01:11:43 +0530

    add active storage

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -5,7 +5,35 @@ ActiveAdmin.register Movie do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :movie_name, :movie_genre, :movie_director, :movie_rating
+  permit_params :movie_name, :movie_genre, :movie_director, :movie_rating, :cover_pic, :display_pic
+
+  show do
+    attributes_table do
+      row :movie_name
+      row :movie_genre
+      row :movie_director
+      row :movie_rating
+      row :cover_pic do |ad|
+        image_tag url_for(ad.cover_pic)
+      end
+      row :display_pic do |dp|
+        image_tag url_for(dp.cover_pic)
+
+    end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :movie_name
+      f.input :movie_genre
+      f.input :movie_director
+      f.input :movie_rating
+      f.input :cover_pic, as: :file
+      f.input :display_pic, as: :file
+    end
+    f.actions
+  end
   #
   # or
   #
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   has_many :watched_movies, dependent: :destroy
   has_many :wish_lists, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  has_one_attached :cover_pic
+  has_one_attached :display_pic
+
 end
