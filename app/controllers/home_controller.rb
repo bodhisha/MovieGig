64f2d88343ece79ddff40c57fcee2c7a1a23d1db
bodhisha <bodhisha@gmail.com>
2020-10-01 02:52:20 +0530

    display most watched movies to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @movies=Movie.all
+    @movies = Movie.all
+    most_watched_movie_ids = WatchedMovie.all.group("movie_id").count.sort_by { |movie, count| -count }.first(3).to_h
+    @sorted_most_watched_movies = Movie.where(id: most_watched_movie_ids.keys)
   end
 end
